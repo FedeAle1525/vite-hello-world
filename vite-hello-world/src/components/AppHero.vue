31e0520587d5d9f463b8bba44e1a12f80badf97c
FedeAle1525 <fedeale.1525@gmail.com>
2023-02-28 16:16:01 +0100

    VUE creation component "AppHero"

---
+++ vite-hello-world/src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<!-- Parte JavaScript -->
+<script>
+export default {
+  data() {
+    return {
+      path: '/Ace.png'
+    }
+  }
+}
+</script>
+
+<!-- Parte HTML  -->
+<template>
+  <div>
+    <img :src="path" alt="">
+  </div>
+</template>
+
+<!-- Parte CSS -->
+<style></style>
