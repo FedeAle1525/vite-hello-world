1fb3efad45f0da8b3f2a2286a9b87c6c76a52443
FedeAle1525 <fedeale.1525@gmail.com>
2023-02-28 15:01:48 +0100

    VUE creation component "AppTitle"

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<!-- Parte JavaScript -->
+<script>
+export default {
+
+  data() {
+    title: 'La mia prima App con Vite!'
+  },
+
+}
+</script>
+
+<!-- Parte HTML -->
+<template>
+  <div>
+    <h1> {{ title }}</h1>
+  </div>
+</template>
+
+<!-- Parte CSS -->
+<style></style>
